cd4481e214b44707a18c4fa77c00d66f227b1490
Franklin Zambrano <franklin.zambrano@kfc.com.ec>
2019-04-29 17:08:05 -0500

    Repositorio Distancia

---
+++ distancia.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float x;
+    int dab=80;
+
+    cout<<"Distancia de Quito a Atacames: 334,1"<<endl;
+    cout<<"Velocidad y tiempo de Juan y su amigo iguales"<<endl;
+    cout<<"Por eso, igualando los datos"<<endl;
+    cout<<"La formula es: 2x=80"<<endl;
+    x=dab/2;
+    cout<<"Puesto que Juan esta en el km 70 se van a encontrar en el km: "+70+x<<endl;
+
+    return 0;
+}
